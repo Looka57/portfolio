debaa21ceccb4fd48f6d5c6b22e7d1ede23826c0
Looka <napoa322@gmail.com>
2024-05-30 12:35:19 +0200

    insertion animation titre principal

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,6 +1,6 @@
 let nuage = document.querySelector("#nuage");
 let montagne = document.querySelector("#montagne");
-// let titre = document.querySelector(".titre");
+let titre = document.querySelector(".titre");
 // let btn = document.querySelector(".btn");
 let temple = document.querySelector("#temple");
 // let header = document.querySelector("header");
@@ -9,7 +9,7 @@ window.addEventListener('scroll', function(){
     let value = window.scrollY;
     nuage.style.top = value * 1.5  + 'px';
     montagne.style.top = value * 0.5  + 'px';
-    // titre.style.transform = 'translateX(' + value * 2 + 'px)';
+    titre.style.transform = 'translateX(' + value * 2 + 'px)';
     // btn.style.top = value * 1  + 'px';
     // header.style.top = value * 0.5  + 'px';
 })
