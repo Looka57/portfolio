f1ee80af237ab50c1f26e2803d5f0fe65789cabe
Looka <napoa322@gmail.com>
2024-05-31 10:29:40 +0200

    Police titre + ajout btn

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,7 +1,7 @@
 let nuage = document.querySelector("#nuage");
 let montagne = document.querySelector("#montagne");
 let titre = document.querySelector(".titre");
-// let btn = document.querySelector(".btn");
+let btn = document.querySelector(".btn");
 let temple = document.querySelector("#temple");
 // let header = document.querySelector("header");
 
@@ -10,7 +10,7 @@ window.addEventListener('scroll', function () {
     nuage.style.top = value * 1.5 + 'px';
     montagne.style.top = value * 0.5 + 'px';
     titre.style.transform = 'translateX(' + value * 2 + 'px)';
-    // btn.style.top = value * 1  + 'px';
+    btn.style.top = value * 1  + 'px';
     // header.style.top = value * 0.5  + 'px';
 })
 
