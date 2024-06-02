1189f0b935602203b2abc105dca045b73184f3c5
Looka <napoa322@gmail.com>
2024-06-02 15:43:56 +0200

    ameioration de la section presentation changement img shinobi

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -3,6 +3,7 @@ let montagne = document.querySelector("#montagne");
 let titre = document.querySelector(".titre");
 let btn = document.querySelector(".btn");
 let temple = document.querySelector("#temple");
+let lune = document.querySelector("#lune");
 // let header = document.querySelector("header");
 
 window.addEventListener('scroll', function () {
@@ -11,7 +12,7 @@ window.addEventListener('scroll', function () {
     montagne.style.top = value * 0.5 + 'px';
     titre.style.transform = 'translateX(' + value * 2 + 'px)';
     btn.style.top = value * 1  + 'px';
-    // header.style.top = value * 0.5  + 'px';
+    lune.style.top = value * .3 + 'px'; 
 })
 
 // background Animation debut
